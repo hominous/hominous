c0210cc9e5b97383fde6d59329f856839efd9b2e
sunba <nsg2318@naver.com>
2021-10-01 22:32:59 +0900

    first commit

---
+++ src/main/java/com/hominous/familiar/FamiliarApplication.java
@@ -0,0 +1,13 @@
+package com.hominous.familiar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamiliarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamiliarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hominous/familiar/FamiliarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hominous.familiar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamiliarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
