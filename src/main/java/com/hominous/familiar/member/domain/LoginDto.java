115a48a6c0a97d8456f8569870f0ff65bc812921
sunba <nsg2318@naver.com>
2021-10-12 20:49:52 +0900

    login -1 Dto 설계

---
+++ src/main/java/com/hominous/familiar/member/domain/LoginDto.java
@@ -0,0 +1,11 @@
+package com.hominous.familiar.member.domain;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class LoginDto {
+    private String userId;
+    private String password;
+}
