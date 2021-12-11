76c8a5038848ac4d20e5ce10bafbc70c3622a82a
sunba <nsg2318@naver.com>
2021-12-11 09:46:44 +0900

    Entity 연관관계 수정

--- src/main/java/com/hominous/familiar/domains/crew/domain/CrewEntity.java
+++ src/main/java/com/hominous/familiar/domains/crew/domain/CrewEntity.java
@@ -1,15 +1,13 @@
 package com.hominous.familiar.domains.crew.domain;
 
 
+import com.hominous.familiar.domains.member.domain.MemberEntity;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -19,6 +17,7 @@ public class CrewEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
 
     private String name;
     private String createdBy;
--- src/main/java/com/hominous/familiar/domains/member/domain/MemberEntity.java
+++ src/main/java/com/hominous/familiar/domains/member/domain/MemberEntity.java
@@ -1,15 +1,13 @@
 package com.hominous.familiar.domains.member.domain;
 
 
+import com.hominous.familiar.domains.crew.domain.CrewEntity;
 import com.sun.istack.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 
 @Entity
@@ -32,6 +30,11 @@ public class MemberEntity {
   private int userMonth;
   @NotNull
   private int userDate;
+
+  //연관관계의 주인은 크루? 멤버?
+  @ManyToOne
+  @JoinColumn(name="crew_id")
+  private CrewEntity crewEntity;
 
   @Builder
   public MemberEntity(String userId, String password, String userName, int userYear, int userMonth, int userDate) {
