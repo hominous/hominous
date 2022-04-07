c25ab3dc6e8d89ebb477c84e943c583eff749272
sunba <nsg2318@naver.com>
2022-04-07 17:01:15 +0900

    add : SecurityConfig Jwt 추가

--- src/main/java/com/hominous/familiar/common/security/SecurityConfig.java
+++ src/main/java/com/hominous/familiar/common/security/SecurityConfig.java
@@ -1,28 +1,81 @@
 package com.hominous.familiar.common.security;
 
 
+import com.hominous.familiar.common.jwt.JwtAccessDeniedHandler;
+import com.hominous.familiar.common.jwt.JwtAuthenticationEntryPoint;
+import com.hominous.familiar.common.jwt.JwtSecurityConfig;
+import com.hominous.familiar.common.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+
+/**
+ * Spring Security 설정클래스
+ *
+ * @EnableGlobalMethodSecurity - @PreAuthorize 어노테이션을 메소드 단위로 추가하기위해서 적용
+ */
+
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-  @Override
-  protected void configure(HttpSecurity httpSecurity) throws Exception{
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
-    httpSecurity
-            .authorizeRequests()
-            .antMatchers("/auth/**").permitAll()
-            .anyRequest().authenticated();
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
 
-  }
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers(
+                        "/h2-console/**",
+                        "/favicon.ico"
+                );
+    }
 
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder(10);
-  }
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                //h2 콘솔용
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                //세션 사용하지 않으므로 (Why not NEVER)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.NEVER)
+
+                .and()
+                .authorizeRequests()
+                .antMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated()
+
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
+
+        ;
+    }
+
 }
