fedadb9fea209ee60d03d531ba3e005300851412
sunba <nsg2318@naver.com>
2022-04-07 17:00:45 +0900

    add : JWT 구현

---
+++ src/main/java/com/hominous/familiar/common/jwt/JwtAccessDeniedHandler.java
@@ -0,0 +1,22 @@
+package com.hominous.familiar.common.jwt;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+/**
+ * 필요한 권한이 존재하지 않는 경우 403에러 리턴하기 위함
+ */
+@Component
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+    }
+}
