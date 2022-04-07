d5c236b52d104dcc313ff22e32dc2f7477deddab
sunba <nsg2318@naver.com>
2022-04-08 02:23:07 +0900

    add : 로그인 컨트롤러, 서비스 jwt토큰기반으로 변경

--- src/main/java/com/hominous/familiar/domain/auth/controller/AuthController.java
+++ src/main/java/com/hominous/familiar/domain/auth/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.hominous.familiar.domain.auth.controller;
 
 
+import com.hominous.familiar.domain.auth.dto.SignInDto;
+import com.hominous.familiar.domain.auth.dto.TokenResponse;
 import com.hominous.familiar.domain.auth.service.AuthService;
 import com.hominous.familiar.domain.auth.dto.SignUpDto;
 import com.hominous.familiar.domain.member.domain.MemberEntity;
@@ -10,22 +12,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
 
-  private final AuthService memberSignupService;
+  private final AuthService authService;
 
   @PostMapping("/auth/sign-up")
   public ResponseEntity<MemberEntity> signUp(
-     @RequestBody SignUpDto memberSignupDto
+     @RequestBody SignUpDto signUpDto
   ) {
-    MemberEntity result = memberSignupService.save(memberSignupDto);
+    MemberEntity result = authService.save(signUpDto);
     return ResponseEntity.ok(result);
   }
 
   @PostMapping("/auth/sign-in")
-  public void signIn() {
-
+  public ResponseEntity<TokenResponse> signIn(@Valid @RequestBody SignInDto signInDto) {
+    return authService.authorize(signInDto);
   }
 }
--- src/main/java/com/hominous/familiar/domain/auth/service/AuthService.java
+++ src/main/java/com/hominous/familiar/domain/auth/service/AuthService.java
@@ -1,10 +1,21 @@
 package com.hominous.familiar.domain.auth.service;
 
 
+import com.hominous.familiar.common.jwt.JwtFilter;
+import com.hominous.familiar.common.jwt.TokenProvider;
+import com.hominous.familiar.domain.auth.dto.SignInDto;
 import com.hominous.familiar.domain.auth.dto.SignUpDto;
+import com.hominous.familiar.domain.auth.dto.TokenResponse;
 import com.hominous.familiar.domain.member.domain.MemberEntity;
 import com.hominous.familiar.domain.member.domain.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,27 +26,41 @@ import java.security.InvalidParameterException;
 @RequiredArgsConstructor
 
 public class AuthService {
-  private final MemberRepository memberRepository;
-  private final PasswordEncoder passwordEncoder;
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenProvider tokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
 
-  @Transactional
-  public MemberEntity save(SignUpDto memberSignupDto) {
+    @Transactional
+    public MemberEntity save(SignUpDto memberSignupDto) {
 
-    String password = memberSignupDto.getPassword();
-    String passwordCheck = memberSignupDto.getPasswordCheck();
+        String password = memberSignupDto.getPassword();
+        String passwordCheck = memberSignupDto.getPasswordCheck();
 
 
-    if (!password.equals(passwordCheck)) {
-      throw new InvalidParameterException("password and passwordCheck inconsistency");
+        if (!password.equals(passwordCheck)) {
+            throw new InvalidParameterException("password and passwordCheck inconsistency");
+        }
+
+        MemberEntity memberEntity = MemberEntity.builder()
+                .memberId(memberSignupDto.getMemberId())
+                .password(passwordEncoder.encode(memberSignupDto.getPassword()))
+                .name(memberSignupDto.getName())
+                .birthdate(memberSignupDto.getBirthdate())
+                .build();
+        return memberRepository.save(memberEntity);
     }
 
-    MemberEntity memberEntity = MemberEntity.builder()
-        .memberId(memberSignupDto.getMemberId())
-        .password(passwordEncoder.encode(memberSignupDto.getPassword()))
-        .name(memberSignupDto.getName())
-        .birthdate(memberSignupDto.getBirthdate())
-        .build();
-    return memberRepository.save(memberEntity);
-  }
+    public ResponseEntity<TokenResponse> authorize(SignInDto signInDto) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(signInDto.getMemberId(), signInDto.getPassword());
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = tokenProvider.createToken(authentication);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer "+ jwt);
+
+        return new ResponseEntity<>(new TokenResponse(jwt),httpHeaders, HttpStatus.OK);
+    }
 }
