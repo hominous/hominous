737bb8c02d329cf5607df9ccf6051b5291732173
sunba <nsg2318@naver.com>
2022-04-10 17:18:38 +0900

    add : 인가 정책 구현(Auth Service 전반)

--- src/main/java/com/hominous/familiar/domain/member/domain/MemberEntity.java
+++ src/main/java/com/hominous/familiar/domain/member/domain/MemberEntity.java
@@ -5,52 +5,44 @@ import com.hominous.familiar.domain.BaseTimeEntity;
 import com.hominous.familiar.domain.auth.domain.AuthorityEntity;
 import com.hominous.familiar.domain.crew.domain.CrewEntity;
 import com.sun.istack.NotNull;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.Set;
 
 
 @Getter
+@Builder
 @Table(name = "member")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Entity
 public class MemberEntity extends BaseTimeEntity {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  @NotNull
-  private String memberId;
-  @NotNull
-  private String password;
-  @NotNull
-  private String name;
-  @NotNull
-  private String birthdate;
+    @NotNull
+    private String memberId;
+    @NotNull
+    private String password;
+    @NotNull
+    private String name;
+    @NotNull
+    private String birthdate;
 
-  @ManyToOne
-  @JoinColumn(name = "crew_id", nullable = true)
-  private CrewEntity crewEntity;
+    @ManyToOne
+    @JoinColumn(name = "crew_id")
+    private CrewEntity crewEntity;
 
-  @ManyToMany
-  @JoinTable(
-          name = "member_authority",
-          joinColumns = {@JoinColumn(name = "member_id",referencedColumnName = "id")},
-          inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")})
-  private Set<AuthorityEntity> authorityEntities;
+    @ManyToMany
+    @JoinTable(
+            name = "member_authority",
+            joinColumns = {@JoinColumn(name = "member_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")})
+    private Set<AuthorityEntity> authorityEntities;
 
-  @Builder
-  public MemberEntity(String memberId, String password, String name, String birthdate) {
-    this.memberId = memberId;
-    this.password = password;
-    this.name = name;
-    this.birthdate = birthdate;
-  }
 }
 
 
