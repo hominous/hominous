82534426993f3a46c9795a9d690423d4f368f7de
sunba <nsg2318@naver.com>
2022-04-11 21:39:52 +0900

    add : Crew Create 관계테이블 추가

--- src/main/java/com/hominous/familiar/domain/member/service/MemberCreateCrewService.java
+++ src/main/java/com/hominous/familiar/domain/member/service/MemberCreateCrewService.java
@@ -1,12 +1,14 @@
 package com.hominous.familiar.domain.member.service;
 
 
+import com.hominous.familiar.domain.auth.domain.AuthorityEntity;
 import com.hominous.familiar.domain.crew.domain.CrewEntity;
 import com.hominous.familiar.domain.crew.domain.CrewRepository;
 import com.hominous.familiar.domain.member.dto.MemberCreateCrewRequest;
 import com.hominous.familiar.domain.member.domain.MemberEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,15 +17,25 @@ public class MemberCreateCrewService {
   private final CrewRepository crewRepository;
   private final MemberFindService memberFindService;
 
+  @Transactional
   public CrewEntity makeCrew(Long memberIndex, MemberCreateCrewRequest memberCreateCrewRequest) {
     MemberEntity memberEntity = memberFindService.findById(memberIndex);
-    // TODO: 2022/04/10 Sign-in 처럼 로직수정 
+
+    if(!crewRepository.existsByCreatedBy(memberEntity.getName())){
+      AuthorityEntity authorityEntity = AuthorityEntity.builder()
+              .authorityName("ROLE_CREW_MAKER")
+              .build();
+      memberEntity.addAuthority(authorityEntity);
+    }
+
     CrewEntity crewEntity = CrewEntity
         .builder()
         .name(memberCreateCrewRequest.getName())
         .createdBy(memberEntity.getName())
         .crewType(memberCreateCrewRequest.getCrewType())
         .build();
-    return crewRepository.save(crewEntity);
+
+    crewRepository.save(crewEntity);
+    return memberEntity.addCrew(crewEntity);
   }
 }
